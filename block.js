5f818415be58fb45218de48232013c848c5de309
unknown <CNAM21@CNAM202021.nicephorecite.local>
2020-12-14 21:29:59 +0100

    Modif de style

---
+++ block.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){     
+    //Dès qu'on clique sur .b1, on applique hide() au titre
+    $(".b2").click(function(){
+        $("#html-box").hide();
+        $("#css-box").show();
+    });
+    
+    //Dès qu'on clique sur .b1, on applique show() au titre
+    $(".b1").click(function(){
+        $("#html-box").show();
+        $("#css-box").hide();
+    });
+});
